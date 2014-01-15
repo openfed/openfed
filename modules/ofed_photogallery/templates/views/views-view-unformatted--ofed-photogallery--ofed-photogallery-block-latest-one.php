88b59dcde90004d8f23637457bcf229254a914b0
Stefan <stefan.r@551886.no-reply.drupal.org>
2014-01-15 12:08:43 +0100

    #2083437: [META] Code quality: warnings in PAreview report

--- modules/ofed_photogallery/templates/views/views-view-unformatted--ofed-photogallery--ofed-photogallery-block-latest-one.php
+++ modules/ofed_photogallery/templates/views/views-view-unformatted--ofed-photogallery--ofed-photogallery-block-latest-one.php
@@ -9,10 +9,10 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<?php foreach ($rows as $id => $row): ?>
-  <div <?php if ($classes_array[$id]) : print 'class="' . $classes_array[$id] . '"';
-  endif; ?>>
+<?php 
+  foreach ($rows as $id => $row): ?>
+    <div <?php if ($classes_array[$id]) : print 'class="' . $classes_array[$id] . '"'; endif; ?>>
     <?php print $row; ?>
   </div>
-<?php endforeach;
-
+<?php
+  endforeach;
